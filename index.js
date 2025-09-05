e8eeb9b361ba3b558bf7db36a8758d76374d765f
sebas777 <sebastiancast06@hotmail.com>
2025-09-05 12:20:39 -0500

    i :rocket:[sc]feat:challenge

--- index.js
+++ index.js
@@ -25,6 +25,35 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+const miLista = document.querySelector(".lista");
+
+function crearLista() {
+  miLista.innerHTML = "";
+
+  cosasQueAprendimos.forEach((cosa) => {
+    const li = document.createElement("li");
+    li.textContent = cosa.tema;
+
+    if (cosa.class && cosa.class !== "special") {
+      li.classList.add(cosa.class);
+    }
+
+    miLista.appendChild(li);
+  });
+}
+
+function eliminarLista() {
+  setTimeout(() => {
+    miLista.innerHTML = "";
+
+    setTimeout(() => {
+      crearLista();
+    }, 3000);
+  }, 5000);
+}
+
+function main() {
+  eliminarLista();
+}
 
 main();
